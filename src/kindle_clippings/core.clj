9885940581fa724ece1146bd3a9ce8cc7b2ca840
paulbowler <paul_bowler@yahoo.com>
2013-09-25 20:52:31 +0100

    Splitting authors

--- src/kindle_clippings/core.clj
+++ src/kindle_clippings/core.clj
@@ -5,6 +5,16 @@
 (def clipping-regex #"(.)*\r\n(.)*\r\n\r\n(.)*\r\n==========\r\n")
 
 (defn clippings-seq
-	"Separates each Kindle clipping in the input text into a sequence"
+	"Separates each Kindle clipping from the input text into a sequence"
 	[txt]
-	(re-seq clipping-regex txt))
+	(first (re-seq clipping-regex txt)))
+	
+(defn clipping-lines
+	"Splits each clipping into its individual lines"
+	[clipping]
+	(clojure.string/split clipping #"\r\n")
+
+(defn authors
+	"Splits the authors section string into individual author names"
+	[authors]
+	(clojure.string/split authors #"(, | and )"))
