88c0b05cac4fef87b486d39ab644cf46f6f181bb
paulbowler <paul_bowler@yahoo.com>
2013-09-25 20:23:56 +0100

    Added regex to split clippings into sequence

--- src/kindle_clippings/core.clj
+++ src/kindle_clippings/core.clj
@@ -1,3 +1,10 @@
 (ns kindle-clippings.core)
   
 (def txt (slurp "resources/My Clippings.txt"))
+
+(def clipping-regex #"(.)*\r\n(.)*\r\n\r\n(.)*\r\n==========\r\n")
+
+(defn clippings-seq
+	"Separates each Kindle clipping in the input text into a sequence"
+	[txt]
+	(re-seq clipping-regex txt))
