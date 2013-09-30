44298e7c407e0d28b034d62548de62ea4e1bede0
paulbowler <paul_bowler@yahoo.com>
2013-09-30 21:01:23 +0100

    Changed version to 1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kindle-clippings "0.1.0-SNAPSHOT"
+(defproject kindle-clippings "1.0"
   :description "A Clojure library to parse the My_Clippings.txt file on Kindle devices"
   :url "https://github.com/paulbowler/kindle-clippings"
   :license {:name "Eclipse Public License"
