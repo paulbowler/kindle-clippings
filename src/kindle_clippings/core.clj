825b4d4bda9b6f08973c7f06950af3d58c4d59c0
paulbowler <paul_bowler@yahoo.com>
2013-09-30 13:47:01 +0100

    Simplified title-author-regex

--- src/kindle_clippings/core.clj
+++ src/kindle_clippings/core.clj
@@ -2,7 +2,9 @@
 
 (def txt (slurp "resources/My Clippings.txt"))
 
-(def clipping-regex #"(.)*\r\n(.)*\r\n\r\n(.)*[?=\r\n==========\r\n]")
+(def clipping-regex #"(.)*\r\n- Highlight (.)*\r\n\r\n(.)*[?=\r\n==========\r\n]")
+
+(def title-author-regex #"^([^\r\n]+) \(([^\r\n\(\)]+)\)$")
 
 (defn clippings-seq
 	"Separates each Kindle clipping from the input text into a sequence"
@@ -12,21 +14,31 @@
 (defn clipping-lines
 	"Splits each clipping into its individual lines"
 	[clipping]
-	(filter #(not (empty? %)) (clojure.string/split (first clipping) #"[\r\n]")))
+  (let [lines (filter #(not (empty? %)) (clojure.string/split (first clipping) #"[\r\n]"))
+        line1 (first lines)
+        line2 (second lines)
+        title (get-title line1)
+        authors (get-authors line1)]
+  {:title title, :authors authors}))
 
-(defn get-authors
+(defn split-authors
 	"Splits the authors section string into individual author names"
 	[authors]
 	(clojure.string/split authors #", | and "))
 
-(defn title-author-line
-  "Split the title and author line into a map of its parts"
+(defn get-title
+  "Get the title from the first line"
 	[line1]
-	(let [v (re-seq #"^([A-Za-z0-9 \(\)-,]+) \(([A-Za-z0-9 ,-]+)\)$" line1)]
-		{:title (nth (first v) 1) :authors (get-authors (last (first v)))}))
+	(second (first (re-seq title-author-regex line1))))
 
-(defn parse []
-  (first (map clipping-lines (clippings-seq txt))))
+(defn get-authors
+  "Get a set of authors from line 1"
+	[line1]
+	(split-authors (last (first (re-seq title-author-regex line1)))))
+
+
+(defn parse [file]
+  (map clipping-lines (clippings-seq file)))
 
 
 
