05ae182838f2f327bc0ff2cd210881e8ca9c29a8
paulbowler <paul_bowler@yahoo.com>
2013-09-30 16:23:17 +0100

    Made lower functions private

--- src/kindle_clippings/core.clj
+++ src/kindle_clippings/core.clj
@@ -1,38 +1,35 @@
 (ns kindle-clippings.core)
 
 (def txt (slurp "resources/My Clippings.txt"))
-
 (def clipping-regex #"(.)*\r\n- Highlight (.)*\r\n\r\n(.)*[?=\r\n==========\r\n]")
-
 (def title-author-regex #"^([^\r\n]+) \(([^\r\n\(\)]+)\)$")
-
 (def meta-regex #"^- Highlight(?: on Page )?([0-9a-z]*)?(?: \|)? Loc. (.*)  \| Added on (.*)$")
 
-(defn clippings-seq
+(defn- clippings-seq
 	"Separates each Kindle clipping from the input text into a sequence"
 	[txt]
 	(re-seq clipping-regex txt))
 
-(defn get-title
+(defn- get-title
   "Get the title from the first line"
 	[line1]
 	(second (first (re-seq title-author-regex line1))))
 
-(defn split-authors
+(defn- split-authors
 	"Splits the authors section string into individual author names"
 	[authors]
 	(clojure.string/split authors #", | and "))
 
-(defn get-authors
+(defn- get-authors
   "Get a set of authors from line 1"
 	[line1]
 	(split-authors (last (first (re-seq title-author-regex line1)))))
 
-(defn get-meta
+(defn- get-meta
   [line2]
   (flatten(re-seq meta-regex line2)))
 
-(defn clipping-lines
+(defn- clipping-lines
 	"Splits each clipping into its individual lines"
 	[clipping]
   (let [lines (filter #(not (empty? %)) (clojure.string/split (first clipping) #"[\r\n]"))
