dc4cc04fd4d1859089dc31a9c8dbfc05dec1183c
paulbowler <paul_bowler@yahoo.com>
2013-09-30 13:58:27 +0100

    Reordered code and added clipping to map

--- src/kindle_clippings/core.clj
+++ src/kindle_clippings/core.clj
@@ -11,31 +11,31 @@
 	[txt]
 	(re-seq clipping-regex txt))
 
-(defn clipping-lines
-	"Splits each clipping into its individual lines"
-	[clipping]
-  (let [lines (filter #(not (empty? %)) (clojure.string/split (first clipping) #"[\r\n]"))
-        line1 (first lines)
-        line2 (second lines)
-        title (get-title line1)
-        authors (get-authors line1)]
-  {:title title, :authors authors}))
+(defn get-title
+  "Get the title from the first line"
+	[line1]
+	(second (first (re-seq title-author-regex line1))))
 
 (defn split-authors
 	"Splits the authors section string into individual author names"
 	[authors]
 	(clojure.string/split authors #", | and "))
 
-(defn get-title
-  "Get the title from the first line"
-	[line1]
-	(second (first (re-seq title-author-regex line1))))
-
 (defn get-authors
   "Get a set of authors from line 1"
 	[line1]
 	(split-authors (last (first (re-seq title-author-regex line1)))))
 
+(defn clipping-lines
+	"Splits each clipping into its individual lines"
+	[clipping]
+  (let [lines (filter #(not (empty? %)) (clojure.string/split (first clipping) #"[\r\n]"))
+        line1 (first lines)
+        line2 (second lines)
+        clipping (nth lines 2)
+        title (get-title line1)
+        authors (get-authors line1)]
+  {:title title, :authors authors, :clipping clipping}))
 
 (defn parse [file]
   (map clipping-lines (clippings-seq file)))
